d95e2d5530d272606356d3dc1480fef8c189d9c9
dmytro.domin <dmytro.domin@skelar.tech>
2026-03-02 00:12:02 +0200

    make reset password page public

--- app/shared/constants/pablic-routes.ts
+++ app/shared/constants/pablic-routes.ts
@@ -1,3 +1,3 @@
 import { ROUTES } from "~/shared/constants/routes";
 
-export const publicRoutes = [ROUTES.LOGIN, ROUTES.CONFIRM, ROUTES.SIGNUP, ROUTES.FORGOT_PASSWORD] as const
+export const publicRoutes = [ROUTES.LOGIN, ROUTES.CONFIRM, ROUTES.SIGNUP, ROUTES.FORGOT_PASSWORD, ROUTES.RESET_PASSWORD] as const
